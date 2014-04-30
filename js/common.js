dd40b9940a8776786ad5483c6d6c674231c1ec42
tuomas56 <pigworts2@gmail.com>
2014-04-30 19:18:20 +0100

    added 3 themes and page-relative fixed dropdown in top-right corner for theme selection

--- js/common.js
+++ js/common.js
@@ -1,5 +1,5 @@
 $(function () {
-    $('a').click(function () {
+    $('.slidea').click(function () {
         $('html, body').animate({
             scrollTop: $($.attr(this, 'href')).offset().top
         }, 1000);
@@ -9,4 +9,16 @@ $(function () {
         evt.preventDefault();
     };
     $("html").bind("dragstart", _preventDefault).bind("selectstart", _preventDefault);
+    $("#cs").click(function () {
+        $(".b2").removeClass("b2").addClass("b1");
+        $(".b3").removeClass("b3").addClass("b1");
+    });
+    $("#ac").click(function () {
+        $(".b1").removeClass("b1").addClass("b2");
+        $(".b3").removeClass("b3").addClass("b2");
+    });
+    $("#cg").click(function () {
+        $(".b1").removeClass("b1").addClass("b3");
+        $(".b2").removeClass("b2").addClass("b3");
+    });
 });
